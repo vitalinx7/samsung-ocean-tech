a922d265dd2a85e5751cdc0f3a4621aec7326dd4
Vitalino Pimenta <118381602+vitalinx7@users.noreply.github.com>
2023-09-21 01:31:36 +0000

    first card commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="card">
+
+        <h2>Rick Sanches</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+
+      </div>      
+    </>
+  )
+}
+
+export default App
