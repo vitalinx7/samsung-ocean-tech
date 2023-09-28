008bbf949e4f698c656bb179281b21b4c0f33fee
Vitalino Pimenta <118381602+vitalinx7@users.noreply.github.com>
2023-09-28 02:23:37 +0000

    tags from api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
-    nome: 'Rick Sanchez',
-    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  const [itens, setItens] = useState([])
+  // useState do React traz duas coisas:
+  // 1. O estado contendo o valor
+  // 2. A função que atualiza o valor do estado
+  //      Quando executada, a função também re-renderiza o componente automaticamente
+
+  async function carregarDadosApi() {
+    const response = await fetch("https://rickandmortyapi.com/api/character/")
+
+    const json = await response.json()
+
+    const results = json.results
+
+    setItens(results)
   }
 
-  const item2 = {
-    nome: 'Morty Smith',
-    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
-
-  const item3 = {
-    nome: 'Summer Smith',
-    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-
-  const itens = [item1, item2, item3]
+  // useEffect recebe duas informações:
+  // 1. Função contendo o código que será protegido pelo useEffect
+  // 2. Dependências que controlam a executam do useEffect.
+  //      Array vazio para executar apenas 1 vez
+  useEffect(
+    function () {
+      carregarDadosApi()
+    },
+    []
+  )
 
   return (
     <>
-      {itens.map(function (item) {
-        return <Card item={item}/>
+      {itens.map(function (item, index) {
+        return <Card item={item} key={`card_${index}`} />
       })}
     </>
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,25 @@
+import Tag from "../Tag/Tag";
 import "./Card.css"
 
 export default function Card(props) {
   const item = props.item;
 
+    const tags = [
+      `Status: ${item.status}`,
+      `Species: ${item.species}`,
+      `Origin: ${item.origin.name}`,
+      
+    ]
+
   return <div className="card">
-    <h2>{item.nome}</h2>
+    <h2>{item.name}</h2>
 
     <div className="tags">
-      <div className="tag">Status: Vivo</div>
-      <div className="tag">Espécie: Humana</div>
-      <div className="tag">Origem: Terra C-137</div>
+      {tags.map(function (tag, index) {
+        return <Tag text={tag} key={`card_tag_${index}`} />
+      })}
     </div>
 
-    <img src={item.imagemUrl} />
+    <img src={item.image} />
   </div>
 }
